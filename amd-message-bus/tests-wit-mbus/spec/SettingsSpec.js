de32f94026f7ebeb7c8c8d81ecf5d2a0b624efd6
Marek Będkowski <marek.bedkowski@pro4people.com>
2013-10-17 21:23:11 +0200

    Updated formatting.

--- amd-message-bus/tests-wit-mbus/spec/SettingsSpec.js
+++ amd-message-bus/tests-wit-mbus/spec/SettingsSpec.js
@@ -21,12 +21,12 @@ describe("Settings", function() {
       it("should register 1 event listeners", function() {
         expect(mBusMock.addEventListener.calls.length).toEqual(1);
       });
-	  it("should register for dao:init event", function() {
-		expect(mBusMock.addEventListener).toHaveBeenCalledWith("dao:init", jasmine.any(Function));
-	  });
-	  it("should read dao settings", function() {
-		mBusMock.addEventListener.calls[0].args[1].call();
-	  });
-	});
+      it("should register for dao:init event", function() {
+        expect(mBusMock.addEventListener).toHaveBeenCalledWith("dao:init", jasmine.any(Function));
+      });
+      it("should read dao settings", function() {
+        mBusMock.addEventListener.calls[0].args[1].call();
+      });
+    });
   });
 });
